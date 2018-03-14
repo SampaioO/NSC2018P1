4e799e6e81f92a36dc98228fd58931c39b3aaf33
Nícolas Sampaio <sampaio.anything@gmail.com>
2018-03-14 12:11:27 -0300

    Site
    Finalização do footer em desktop

--- views/componentes/footer.php
+++ views/componentes/footer.php
@@ -1,6 +1,7 @@
 <style type="text/css">
-	.session-blocos-rodape .row .col-primaria { background-color: #1e1e1e; height: 265px }
-	.session-blocos-rodape .row .col-secundaria { background-color: #171717; height: 265px }
+	@media screen and (min-width: 768px) {
+		body { background: url('<?= RAIZSITE ?>/img/marcacao-fundo.jpg'); background-position: center center }
+	}
 
 	@font-face {
         font-family:"Geared Slab";
@@ -10,17 +11,27 @@
         src:url("<?= RAIZSITE ?>/fonts/GearedSlab.ttf");
     }
 
+	.row-alternativa { display: table; margin-left: 0; margin-right: 0; width: 100% }
+	.session-blocos-rodape .row-alternativa { position: absolute; margin-top: -265px }
+	.session-blocos-rodape .row-alternativa .col-primaria { background-color: #1e1e1e; height: 265px }
+	.session-blocos-rodape .row-alternativa .col-secundaria { background-color: #171717; height: 265px }
+
+    footer { position: relative }
+    footer .row { position: relative; height: 265px; z-index: 1 }
     footer .bloco-links { line-height: 25px }
-	footer .bloco-links a { font-family: "Geared Slab"; letter-spacing:1px; color: #000000; font-weight: 800; text-transform: uppercase }
+	footer .bloco-links a { font-family: "Geared Slab"; letter-spacing:1px; color: #ffffff; font-weight: 800; text-transform: uppercase }
 	footer .bloco-links a:hover { color: #ffbc1b }
 
+	footer .bloco-logo,
+	footer .bloco-links,
+	footer .bloco-form { position: relative; height: 265px }
 	footer .bloco-form { font-family: 'Open Sans', sans-serif }
-	footer .bloco-form p.chamada { color: #f8a41a; font-size: 10px }
+	footer .bloco-form p.chamada { color: #f8a41a; font-size: 10px; margin-top: 10px }
 	footer .bloco-form p.endereco { color: #acacac; font-size: 10px }
-
-	footer .bloco-form form input,
+	footer .bloco-form form input { height: 31.5px; border-radius: 0px; color: #acacac; border-color: transparent }
 	footer .bloco-form form button { height: 30px; border-radius: 0px }
-	footer .bloco-form form button { padding: 0px 12px; background-color: #f8a41a; color: #ffffff; border-color: #f8a41a }
+	footer .bloco-form form input { background-color: #1e1e1e }
+	footer .bloco-form form button { padding: 0px 12px; background-color: #f8a41a; color: #000000; border-color: #f8a41a; font-weight: 600; text-transform: uppercase }
 	footer .bloco-form form button:active,
 	footer .bloco-form form button:focus,
 	footer .bloco-form form button:active:focus,
@@ -29,38 +40,46 @@
 <footer>
 	<div class="container">
 		<div class="row">
-			<div class="col-lg-2 col-md-2 col-sm-2 hidden-xs"><img src="<?= RAIZSITE ?>/img/marcacao-rodape-1.png" class="img-responsive"></div>
+			<div class="col-lg-2 col-md-2 col-sm-2 hidden-xs bloco-logo">
+				<div class="position-center-vertical">
+					<img src="<?= RAIZSITE ?>/img/marcacao-rodape-1.png" class="img-responsive"></div>
+				</div>
 			<div class="col-lg-1 col-md-1 col-sm-1 hidden-xs">&nbsp;</div>
 			<div class="col-lg-3 col-md-3 col-sm-3 hidden-xs bloco-links">
-				<a href="" class="link-default">A Club</a><br>
-				<a href="" class="link-default">Motos</a><br>
-				<a href="" class="link-default">Blog</a><br>
-				<a href="" class="link-default">Contato</a>
+				<div class="position-center-vertical">
+					<a href="" class="link-default">A Club</a><br>
+					<a href="" class="link-default">Motos</a><br>
+					<a href="" class="link-default">Blog</a><br>
+					<a href="" class="link-default">Contato</a>
+				</div>
 			</div>
 
 			<div class="col-lg-1 col-md-1 col-sm-1 hidden-xs">&nbsp;</div>
 			<div class="col-lg-5 col-md-5 col-sm-5 hidden-xs bloco-form">
-				<form action="" class="form-horizontal" method="post">
-					<div class="input-group">
-						<input type="text" class="form-control" placeholder="Search for...">
-						<span class="input-group-btn">
-							<button class="btn btn-default" type="button">Enviar!</button>
-						</span>
-					</div>
-				</form>
-				<p class="chamada">Esteja atualizado sobre os novos produtos, lançamentos, promoções e muito mais!</p>
-				<p class="endereco">Alto da XV: RUA DIAS DA ROCHA FILHO 79, ESQUINA COM A SETE DE ABRIL<br>CURITIBA | PARANÁ | 41 3029 0095 | 41 3057 1782</p>
-				<p class="endereco">Distrito 1340: RUA MAJOR HEITOR GUIMARÃES, 1130 - SEMINÁRIO<br>CURITIBA | PARANÁ | 41 3057 1782</p>
+				<div class="position-center-vertical">
+					<form action="" class="form-horizontal" method="post">
+						<div class="input-group">
+							<input type="text" class="form-control" placeholder="Digite aqui seu e-mail...">
+							<span class="input-group-btn">
+								<button class="btn btn-default" type="button">Enviar!</button>
+							</span>
+						</div>
+					</form>
+					<p class="chamada">Esteja atualizado sobre os novos produtos, lançamentos, promoções e muito mais!</p>
+					<p class="endereco"><strong>Alto da XV:</strong> RUA DIAS DA ROCHA FILHO 79, ESQUINA COM A SETE DE ABRIL | CURITIBA | PARANÁ | 41 3029 0095 | 41 3057 1782</p>
+					<p class="endereco"><strong>Distrito 1340:</strong> RUA MAJOR HEITOR GUIMARÃES, 1130 - SEMINÁRIO | CURITIBA | PARANÁ | 41 3057 1782</p>
+				</div>
 			</div>
 		</div>
 	</div>
-</footer>
-<div class="session-blocos-rodape">
-	<div class="row">
-		<div class="col-lg-6 col-md-6 col-sm-6 hidden-xs col-primaria">&nbsp;</div>
-		<div class="col-lg-6 col-md-6 col-sm-6 hidden-xs col-secundaria">&nbsp;</div>
+
+	<div class="session-blocos-rodape">
+		<div class="row-alternativa">
+			<div class="col-lg-6 col-md-6 col-sm-6 hidden-xs col-primaria">&nbsp;</div>
+			<div class="col-lg-6 col-md-6 col-sm-6 hidden-xs col-secundaria">&nbsp;</div>
+		</div>
 	</div>
-</div>
+</footer>
 
 </body>
 </html>
