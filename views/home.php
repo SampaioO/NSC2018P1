b2025e51a55c72d7748add4af4ae8bdd841c9cfb
Nícolas Sampaio <sampaio.anything@gmail.com>
2018-03-19 15:01:59 -0300

    Site - Home
    Iniciando ajustes e inclusão de conteúdo na home

--- views/home.php
+++ views/home.php
@@ -1,1 +1,116 @@
-HOME
+<style type="text/css">
+	.center { display: block !important; text-align: center !important; text-align: -webkit-center !important }
+
+	.titulo { font-family: 'Roboto', sans-serif; font-weight: 600; text-transform: uppercase; color: #000000; background-color: #ffbc1b; border-radius: 15px; font-size: 22px; padding-top: 10px; padding-bottom: 10px; margin-bottom: 30px }
+
+	.div-moto { margin-bottom: 60px }
+	.div-moto .moto-informacoes { margin-top: 12px; margin-bottom: 8px }
+	.div-moto .moto-informacoes p { font-family: 'Roboto', sans-serif; color: #58595b; margin: 0 }
+	.div-moto .moto-informacoes p.valor { font-weight: 500 }
+	.div-moto .moto-informacoes p.nome,
+	.div-moto .moto-informacoes p.ano { font-size: 12px }
+	.div-moto .moto-informacoes p.valor { margin-top: 8px }
+
+	a.btn-detalhes { font-family: 'Roboto', sans-serif; font-weight: 500 !important; text-transform: uppercase; border-radius: 0 !important; font-size: 10px !important ; margin-left: auto; margin-right: auto; width: 180px }
+	a.btn-detalhes { background-color: #ffbc1b; color: #000000; border-color: #ffbc1b }
+	a.btn-detalhes:hover,
+	a.btn-detalhes:focus,
+	a.btn-detalhes:active:hover { background-color: transparent; color: #ffbc1b; border-color: #ffbc1b }
+</style>
+<div class="container" style="background-color: #ffffff">
+	<div class="row">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div class="titulo center">novidades na club</div>
+		</div>
+
+		<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 div-moto">
+			<div style="height: 156px; background: url('<?= RAIZSITE ?>/img/marcacao-moto.png'); width: 100%; border-radius: 15px; background-size: 100%"></div>
+
+			<div class="moto-informacoes center">
+				<p class="nome">HARLEY-DAVIDSON BREAKOUT</p>
+				<p class="ano">2016/2016</p>
+				<p class="valor">R$ 57.900,00</p>
+			</div>
+
+			<a role="button" class="btn btn-detalhes center" href="">Ver detalhes</a>
+		</div>
+		<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 div-moto">
+			<div style="height: 156px; background: url('<?= RAIZSITE ?>/img/marcacao-moto.png'); width: 100%; border-radius: 15px; background-size: 100%"></div>
+
+			<div class="moto-informacoes center">
+				<p class="nome">HARLEY-DAVIDSON BREAKOUT</p>
+				<p class="ano">2016/2016</p>
+				<p class="valor">R$ 57.900,00</p>
+			</div>
+
+			<a role="button" class="btn btn-detalhes center" href="">Ver detalhes</a>
+		</div>
+		<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 div-moto">
+			<div style="height: 156px; background: url('<?= RAIZSITE ?>/img/marcacao-moto.png'); width: 100%; border-radius: 15px; background-size: 100%"></div>
+
+			<div class="moto-informacoes center">
+				<p class="nome">HARLEY-DAVIDSON BREAKOUT</p>
+				<p class="ano">2016/2016</p>
+				<p class="valor">R$ 57.900,00</p>
+			</div>
+
+			<a role="button" class="btn btn-detalhes center" href="">Ver detalhes</a>
+		</div>
+		<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 div-moto">
+			<div style="height: 156px; background: url('<?= RAIZSITE ?>/img/marcacao-moto.png'); width: 100%; border-radius: 15px; background-size: 100%"></div>
+
+			<div class="moto-informacoes center">
+				<p class="nome">HARLEY-DAVIDSON BREAKOUT</p>
+				<p class="ano">2016/2016</p>
+				<p class="valor">R$ 57.900,00</p>
+			</div>
+
+			<a role="button" class="btn btn-detalhes center" href="">Ver detalhes</a>
+		</div>
+
+		<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 div-moto">
+			<div style="height: 156px; background: url('<?= RAIZSITE ?>/img/marcacao-moto.png'); width: 100%; border-radius: 15px; background-size: 100%"></div>
+
+			<div class="moto-informacoes center">
+				<p class="nome">HARLEY-DAVIDSON BREAKOUT</p>
+				<p class="ano">2016/2016</p>
+				<p class="valor">R$ 57.900,00</p>
+			</div>
+
+			<a role="button" class="btn btn-detalhes center" href="">Ver detalhes</a>
+		</div>
+		<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 div-moto">
+			<div style="height: 156px; background: url('<?= RAIZSITE ?>/img/marcacao-moto.png'); width: 100%; border-radius: 15px; background-size: 100%"></div>
+
+			<div class="moto-informacoes center">
+				<p class="nome">HARLEY-DAVIDSON BREAKOUT</p>
+				<p class="ano">2016/2016</p>
+				<p class="valor">R$ 57.900,00</p>
+			</div>
+
+			<a role="button" class="btn btn-detalhes center" href="">Ver detalhes</a>
+		</div>
+		<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 div-moto">
+			<div style="height: 156px; background: url('<?= RAIZSITE ?>/img/marcacao-moto.png'); width: 100%; border-radius: 15px; background-size: 100%"></div>
+
+			<div class="moto-informacoes center">
+				<p class="nome">HARLEY-DAVIDSON BREAKOUT</p>
+				<p class="ano">2016/2016</p>
+				<p class="valor">R$ 57.900,00</p>
+			</div>
+
+			<a role="button" class="btn btn-detalhes center" href="">Ver detalhes</a>
+		</div>
+		<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 div-moto">
+			<div style="height: 156px; background: url('<?= RAIZSITE ?>/img/marcacao-moto.png'); width: 100%; border-radius: 15px; background-size: 100%"></div>
+
+			<div class="moto-informacoes center">
+				<p class="nome">HARLEY-DAVIDSON BREAKOUT</p>
+				<p class="ano">2016/2016</p>
+				<p class="valor">R$ 57.900,00</p>
+			</div>
+
+			<a role="button" class="btn btn-detalhes center" href="">Ver detalhes</a>
+		</div>
+	</div>
+</div>
