1e94460fd7fe902343db567d06c8b48e5a4c7949
Nícolas Sampaio <sampaio.anything@gmail.com>
2018-03-27 11:59:27 -0300

    Site - Harley Davidson Curitiba
    Ajustes finos realizados no conteúdo principal

--- views/harley-davidson-curitiba.php
+++ views/harley-davidson-curitiba.php
@@ -31,22 +31,37 @@
 <style type="text/css">
 	.session-conteudo { background-color: #ffffff }
 	.session-conteudo .bloco-conteudo p { font-family: 'Roboto', sans-serif; color: #4b4b4b }
+
+	h1,
+	h2,
+	h3 { font-family: 'Geared Slab' !important; margin: 0 !important }
 </style>
 <div class="container session-conteudo">
 	<div class="row">
-		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 bloco-conteudo">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="background-color: #000000">
+			<h1 style="color: #f9a61a; text-transform: uppercase; font-weight: 600; font-size: 44px; margin-left: 32px !important; padding-top: 10px; padding-bottom: 10px">Club 1903 motorcycles,</h1>
+		</div>
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<h2 style="color: #4b4b4b; text-transform: uppercase; font-size: 29px; margin-left: 32px !important; padding-top: 10px; padding-bottom: 10px">PRA QUEM VIVE A EMOÇÃO DO ESTILO HARLEY DAVIDSON.</h2>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 bloco-conteudo" style="padding-top: 40px; padding-bottom: 30px">
 			<p>A Club 1903 Motorcycles é o ponto de encontro dos amantes da Harley Davidson em Curitiba. O lugar é ideal para quem gosta de viver com o coração acelerado e respiração ao vento. Afinal, muito mais do que a marca mais amada de motocicletas, a Harley Davidson é um estilo de vida. O nome Club 1903 Motorcycles é uma homenagem ao ano de criação da lendária marca. Na época, dois jovens de Milwaukee, nos Estados Unidos, resolvem instalar um motor em uma bicicleta, com a intenção de se locomover mais de forma mais rápida e cômoda nas subidas.</p>
 			<p>Estes dois jovens se chamavam William S. Harley e Arthur Davidson. Mal sabiam eles que estavam criando um dos meios de transporte mais utilizados do mundo e uma das marcas mais amadas de todos os tempos, a Harley Davidson. Em pouco tempo, a notoriedade da marca cresceu e ultrapassou os limites da região onde viviam os jovens. No ano de 1907 foram produzidas pelo menos cem máquinas.</p>
 			<p>Para promover a marca e comprovar a qualidade a fim de fazer a demonstração flagrante da qualidade das motos doravante fabricadas, Walter Davidson envolve-se de desporto motociclista e alcança a vitória, em 1907, numa famosa corrida de resistência. Assim, a marca Harley conta com mais de 100 anos de uma história marcada por inovação, aventura e paixão tornou-se uma lenda no mundo inteiro. Em homenagem a essa trajetória, a Club 1903 Motorcycles foi criada em um espaço amplo e personalizado, a fim de reunir o espírito de liberdade da Harley Davidson e os admiradores das motocicletas mais desejadas do mundo.</p>
 			<p>A Club 1903 Motorcycles oferece modelos seminovos das linhas Sportster, Dyna, Softail, Touring, V-Rod. Na loja você também encontra diversos acessórios Harley Davidson, peças, vestuário, capacetes, luvas, viseiras e muito mais. Nossa missão é unir bons amigos para continuar buscando por aventuras cada vez melhores.</p>
 		</div>
 	</div>
-	<div class="row">
-		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-			<img src="<?= RAIZSITE ?>/img/marcacao-auto-xv.jpg" class="img-responsive">
+	<div class="row" style="padding-top: 25px; padding-bottom: 50px">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<h3 style="color: #f9a61a; text-transform: uppercase; font-weight: 600; font-size: 44px; margin-left: 32px !important">Tour 360º</h3>
 		</div>
 		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-			<img src="<?= RAIZSITE ?>/img/marcacao-distrito-1340.jpg" class="img-responsive">
+			<div style="background: url('<?= RAIZSITE ?>/img/marcacao-auto-xv.jpg'); background-size: cover; background-position: center center; border-radius: 15px; height: 265px; width: 100%"></div>
+		</div>
+		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+			<div style="background: url('<?= RAIZSITE ?>/img/marcacao-distrito-1340.jpg'); background-size: cover; background-position: center center; border-radius: 15px; height: 265px; width: 100%"></div>
 		</div>
 	</div>
 </div>
