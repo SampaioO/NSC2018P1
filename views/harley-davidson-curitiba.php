feb6c7fe778ad176ce3f5d809244fa2b9ffb8100
Nícolas <nicolas@comberweb.com.br>
2018-04-03 06:17:54 -0300

    Site - Harley Davidson Curitiba
    Código organizado e melhorias foram feitos no layout da página

--- views/harley-davidson-curitiba.php
+++ views/harley-davidson-curitiba.php
@@ -6,7 +6,6 @@
 <style type="text/css">
 	@media screen and (min-width: 768px) {
 		.bg-carousel { height: 500px }
-		.bg-destaque { height: 245px }
 		.session-banner,
 		.session-destaque,
 		.session-logos,
@@ -19,7 +18,6 @@
 		.session-banner,
 		.session-destaque { padding-left: 15px !important; padding-right: 15px !important }
 		.bg-carousel { height: 250px; background-size: cover !important }
-		.bg-destaque { height: 145px }
 	}
 </style>
 <div class="container session-banner">
@@ -37,50 +35,129 @@
 	h3 { font-family: 'Geared Slab' !important; margin: 0 !important }
 </style>
 <div class="container session-conteudo">
-	<div class="row">
-		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="background-color: #000000">
-			<h1 style="color: #f9a61a; text-transform: uppercase; font-weight: 600; font-size: 44px; margin-left: 32px !important; padding-top: 10px; padding-bottom: 10px">Club 1903 motorcycles,</h1>
+	<style type="text/css">
+		.row-harley-principal h1 { color: #f9a61a; text-transform: uppercase; font-weight: 600; font-size: 44px }
+		.row-harley-principal h2 { color: #4b4b4b; text-transform: uppercase; font-size: 29px }
+
+		@media screen and (max-width: 767px) {}
+
+		@media screen and (min-width: 768px) and (max-width: 991px) {}
+		@media screen and (min-width: 992px) and (max-width: 1199px) {}
+
+		@media screen and (min-width: 768px) {
+			.row-harley-principal div:first-child { background-color: #000000 }
+
+			.row-harley-principal h1 { margin-left: 32px !important; padding-top: 10px; padding-bottom: 10px }
+			.row-harley-principal h2 { margin-left: 32px !important; padding-top: 10px; padding-bottom: 10px }
+
+			.row-harley-conteudo .bloco-conteudo { padding-top: 40px; padding-bottom: 30px }
+		}
+		@media screen and (min-width: 992px) {}
+		@media screen and (min-width: 1200px) {}
+	</style>
+	<div class="row row-harley-principal">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<h1>Club 1903 motorcycles,</h1>
 		</div>
 		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-			<h2 style="color: #4b4b4b; text-transform: uppercase; font-size: 29px; margin-left: 32px !important; padding-top: 10px; padding-bottom: 10px">PRA QUEM VIVE A EMOÇÃO DO ESTILO HARLEY DAVIDSON.</h2>
+			<h2>PRA QUEM VIVE A EMOÇÃO DO ESTILO HARLEY DAVIDSON.</h2>
 		</div>
 	</div>
-	<div class="row">
-		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 bloco-conteudo" style="padding-top: 40px; padding-bottom: 30px">
+	<div class="row row-harley-conteudo">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 bloco-conteudo">
 			<p>A Club 1903 Motorcycles é o ponto de encontro dos amantes da Harley Davidson em Curitiba. O lugar é ideal para quem gosta de viver com o coração acelerado e respiração ao vento. Afinal, muito mais do que a marca mais amada de motocicletas, a Harley Davidson é um estilo de vida. O nome Club 1903 Motorcycles é uma homenagem ao ano de criação da lendária marca. Na época, dois jovens de Milwaukee, nos Estados Unidos, resolvem instalar um motor em uma bicicleta, com a intenção de se locomover mais de forma mais rápida e cômoda nas subidas.</p>
 			<p>Estes dois jovens se chamavam William S. Harley e Arthur Davidson. Mal sabiam eles que estavam criando um dos meios de transporte mais utilizados do mundo e uma das marcas mais amadas de todos os tempos, a Harley Davidson. Em pouco tempo, a notoriedade da marca cresceu e ultrapassou os limites da região onde viviam os jovens. No ano de 1907 foram produzidas pelo menos cem máquinas.</p>
 			<p>Para promover a marca e comprovar a qualidade a fim de fazer a demonstração flagrante da qualidade das motos doravante fabricadas, Walter Davidson envolve-se de desporto motociclista e alcança a vitória, em 1907, numa famosa corrida de resistência. Assim, a marca Harley conta com mais de 100 anos de uma história marcada por inovação, aventura e paixão tornou-se uma lenda no mundo inteiro. Em homenagem a essa trajetória, a Club 1903 Motorcycles foi criada em um espaço amplo e personalizado, a fim de reunir o espírito de liberdade da Harley Davidson e os admiradores das motocicletas mais desejadas do mundo.</p>
 			<p>A Club 1903 Motorcycles oferece modelos seminovos das linhas Sportster, Dyna, Softail, Touring, V-Rod. Na loja você também encontra diversos acessórios Harley Davidson, peças, vestuário, capacetes, luvas, viseiras e muito mais. Nossa missão é unir bons amigos para continuar buscando por aventuras cada vez melhores.</p>
 		</div>
 	</div>
-	<div class="row" style="padding-top: 25px; padding-bottom: 100px">
+
+	<style type="text/css">
+		.row-harley-tour h3 { color: #f9a61a; text-transform: uppercase; font-weight: 600; font-size: 44px }
+
+		.div-tour { position: relative; -webkit-transition: all 0.5s ease-out; transition: all 0.5s ease-out }
+
+		@media screen and (max-width: 767px) {
+			.div-tour { margin-bottom: 80px }
+		}
+
+		@media screen and (min-width: 768px) and (max-width: 991px) {
+			.div-tour .destaque { width: 345px }
+		}
+		@media screen and (min-width: 992px) and (max-width: 1199px) {}
+
+		@media screen and (min-width: 768px) {
+			.row-harley-tour { padding-top: 25px; padding-bottom: 100px }
+			.row-harley-tour h3 { margin-left: 32px !important }
+
+			.div-tour .destaque { top: 0; -webkit-transition: all 0.5s ease-out; transition: all 0.5s ease-out }
+			.div-tour:hover .destaque { top: 45px; -webkit-transition: all 0.5s ease-out; transition: all 0.5s ease-out }
+		}
+		@media screen and (min-width: 992px) {
+			.div-tour .destaque { width: 450px }
+		}
+		@media screen and (min-width: 1200px) {}
+	</style>
+	<div class="row row-harley-tour">
 		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-			<h3 style="color: #f9a61a; text-transform: uppercase; font-weight: 600; font-size: 44px; margin-left: 32px !important">Tour 360º</h3>
+			<h3>Tour 360º</h3>
 		</div>
-		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12" style="position: relative">
-			<div style="background-color: #000000; position: relative; height: 265px; border-radius: 15px; text-align: center">
-				<p style="text-transform: uppercase; color: #ffbc1b; font-size: 16px; padding-top: 11px">Alto da XV</p>
-			</div>
+		<a href="" target="_blank">
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 div-tour">
+				<div class="fundo" style="background-color: #000000; position: relative; height: 265px; border-radius: 15px; text-align: center">
+					<p style="text-transform: uppercase; color: #ffbc1b; font-size: 16px; padding-top: 11px">Alto da XV</p>
 
-			<div style="background: url('<?= RAIZSITE ?>/img/marcacao-auto-xv.jpg'); background-size: cover; background-position: center center; border-radius: 15px; height: 265px; width: 450px; position: absolute; top: 45px"></div>
-		</div>
-		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12" style="position: relative">
-			<div style="background-color: #000000; position: relative; height: 265px; border-radius: 15px; text-align: center">
-				<p style="text-transform: uppercase; color: #ffbc1b; font-size: 16px; padding-top: 11px">Distrito 1340</p>
-			</div>
+					<div class="destaque hidden-lg hidden-md hidden-sm" style="background: url('<?= RAIZSITE ?>/img/marcacao-auto-xv.jpg'); background-size: cover; background-position: center center; border-radius: 15px; height: 265px; position: absolute; width: 100%"></div>
+				</div>
 
-			<div style="background: url('<?= RAIZSITE ?>/img/marcacao-distrito-1340.jpg'); background-size: cover; background-position: center center; border-radius: 15px; height: 265px; width: 450px; position: absolute; top: 45px"></div>
-		</div>
+				<div class="destaque hidden-xs" style="background: url('<?= RAIZSITE ?>/img/marcacao-auto-xv.jpg'); background-size: cover; background-position: center center; border-radius: 15px; height: 265px; position: absolute"></div>
+			</div>
+		</a>
+		<a href="" target="_blank">
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 div-tour">
+				<div class="fundo" style="background-color: #000000; position: relative; height: 265px; border-radius: 15px; text-align: center">
+					<p style="text-transform: uppercase; color: #ffbc1b; font-size: 16px; padding-top: 11px">Distrito 1340</p>
+
+					<div class="destaque hidden-lg hidden-md hidden-sm" style="background: url('<?= RAIZSITE ?>/img/marcacao-distrito-1340.jpg'); background-size: cover; background-position: center center; border-radius: 15px; height: 265px; position: absolute; width: 100%"></div>
+				</div>
+
+				<div class="destaque hidden-xs" style="background: url('<?= RAIZSITE ?>/img/marcacao-distrito-1340.jpg'); background-size: cover; background-position: center center; border-radius: 15px; height: 265px; position: absolute; top: 45px"></div>
+			</div>
+		</a>
 	</div>
 </div>
 
+<style type="text/css">
+	.row-harley-destaque .bg-destaque { background: url('<?= RAIZSITE ?>/img/marcacao-sessao-club.jpg'); background-position: center center; background-size: cover; position: relative }
+	.row-harley-destaque .bg-destaque div { background-color: #25211a; position: absolute; right: 0; padding: 15px 15px }
+
+	.row-harley-destaque .bg-destaque h3 { color: #f9a61a; font-size: 30px }
+	.row-harley-destaque .bg-destaque p { font-family: 'Roboto', sans-serif; color: #ffffff }
+
+	@media screen and (max-width: 767px) {
+		.row-harley-destaque .bg-destaque { height: 270px }
+	}
+
+	@media screen and (min-width: 768px) and (max-width: 991px) {}
+	@media screen and (min-width: 992px) and (max-width: 1199px) {}
+
+	@media screen and (min-width: 768px) {
+		.row-harley-destaque .bg-destaque { height: 245px }
+		
+		.row-harley-destaque .bg-destaque div { border-top-left-radius: 15px; border-bottom-left-radius: 15px }
+
+		.row-harley-destaque .bg-destaque h3 { margin-bottom: 10px !important }
+	}
+	@media screen and (min-width: 992px) {}
+	@media screen and (min-width: 1200px) {}
+</style>
 <div class="container session-destaque">
-	<div class="row">
-		<div class="bg-destaque" style="background: url('<?= RAIZSITE ?>/img/marcacao-sessao-club.jpg'); background-position: center center; background-size: cover; position: relative">
-			<div class="col-lg-5 col-md-5 col-sm-5 col-xs-12 position-center-vertical" style="background-color: #25211a; border-top-left-radius: 15px; border-bottom-left-radius: 15px; position: absolute; right: 0; padding: 15px 15px">
-				<h3 style="color: #f9a61a; font-size: 30px">MOTO HARLEY-DAVIDSON<br>EM CURITIBA É NA CLUB 1903</h3>
-				<p style="font-family: 'Roboto', sans-serif; color: #ffffff; margin-top: 10px">Encontre a moto Harley Davidson que vai mudar a sua vida. Confira os modelos Sportster, Dyna, Softail, Touring, V-Rod e escolha o modelo que mais combina com o seu estilo de vida.</p>
-				<p style="font-family: 'Roboto', sans-serif; color: #ffffff">A estrada espera por você.</p>
+	<div class="row row-harley-destaque">
+		<div class="bg-destaque">
+			<div class="col-lg-5 col-md-5 col-sm-6 col-xs-12 position-center-vertical">
+				<h3>MOTO HARLEY-DAVIDSON<br>EM CURITIBA É NA CLUB 1903</h3>
+				<p>Encontre a moto Harley Davidson que vai mudar a sua vida. Confira os modelos Sportster, Dyna, Softail, Touring, V-Rod e escolha o modelo que mais combina com o seu estilo de vida.</p>
+				<p>A estrada espera por você.</p>
 			</div>
 		</div>
 	</div>
