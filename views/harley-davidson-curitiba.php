b08af53cf13c820f9d502d1396ed89f8e4eca0e6
Nícolas <nicolas@comberweb.com.br>
2018-04-03 16:35:17 -0300

    Site - Harley Davidson Curitiba
    Finalização da sessão de modelos, iniciando ajustes finos da página.

--- views/harley-davidson-curitiba.php
+++ views/harley-davidson-curitiba.php
@@ -230,13 +230,22 @@
     </div>
 
     <style type="text/css">
-	    .session-modelos .div-conteudo .conteudo { text-align: justify; color: #4b4b4b }
-	    .session-modelos .div-conteudo .conteudo p { margin-bottom: 16px }
+		.rotate { -webkit-transform: rotate(-180deg); -moz-transform: rotate(-180deg); -ms-transform: rotate(-180deg); -o-transform: rotate(-180deg); filter: progid:DXImageTransform.Microsoft.BasicImage(rotation=3); }
+
+	    .row-modelo .conteudo { text-align: justify; color: #4b4b4b }
+	    .row-modelo .conteudo p { margin-bottom: 16px }
+
+	    .row-modelo { margin-bottom: 50px }
+	    .row-modelo .titulo-harley { font-family: 'Geared Slab' !important; margin: 0 !important; font-size: 20px; color: #f9a61a; text-transform: uppercase }
+	    .row-modelo .subtitulo-harley { font-family: 'Geared Slab' !important; margin: 0 !important; font-size: 45px; margin-top: -10px !important; color: #f9a61a }
+
+	    .btn-modelo { font-family: 'Roboto', sans-serif !important; font-weight: 700 !important }
+	    .btn-modelo { bottom: 15px; background-color: #f7a600; border-radius: 8px; text-transform: uppercase; font-size: 18px; color: #000000; padding: 8px 25px !important }
 	</style>
-    <div class="row" style="margin-bottom: 50px">
-		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 div-conteudo">
-			<h4 style="font-family: 'Geared Slab' !important; margin: 0 !important; font-size: 20px; color: #f9a61a; text-transform: uppercase">harley-davidson</h4>
-			<p style="font-family: 'Geared Slab' !important; margin: 0 !important; font-size: 45px; margin-top: -10px !important; color: #f9a61a">DYNA</p>
+    <div class="row row-modelo">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+			<h4 class="titulo-harley">harley-davidson</h4>
+			<p class="subtitulo-harley">DYNA</p>
 			
 			<div class="Open-Sans Light size14 Preto-Fonte conteudo">
 				<p>Uma das principais características das motocicletas da família Dyna é a suspensão traseira bichoque visível, que conecta a balança à caixa de marchas. Essa a diferença da família Softail que tem sua suspensão escondida sobre a caixa de marchas. A linha possui algumas customizações, dentre elas é possível citar a Dyna SuperGlide, SuperGlide Custom e a WideGlide.</p>
@@ -245,7 +254,150 @@
 			</div>
 		</div>
 
-		<div class="hidden-xs hidden-sm hidden-md col-lg-6" style="height: 530px">
+		<div class="hidden-xs hidden-sm hidden-md col-lg-6 div-tour" style="margin-top: 40px">
+			<div class="fundo" style="background-color: #000000; position: relative; height: 265px; border-radius: 15px; text-align: center">
+				<svg class="rotate" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" width="20px" height="20px" viewBox="0 0 357 357" style="enable-background:new 0 0 357 357; position: absolute; top: 20px; left: 25px" xml:space="preserve"><g><g id="play-arrow"><polygon points="38.25,0 38.25,357 318.75,178.5   " fill="#f7a600"/></g></g></svg>
+
+				<p style="font-family: 'Geared Slab' !important; text-transform: uppercase; color: #ffbc1b; font-size: 20px; padding-top: 18px">Super glide custom</p>
+
+				<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" width="20px" height="20px" viewBox="0 0 357 357" style="enable-background:new 0 0 357 357; position: absolute; top: 20px; right: 25px" xml:space="preserve"><g><g id="play-arrow"><polygon points="38.25,0 38.25,357 318.75,178.5   " fill="#f7a600"/></g></g></svg>
+
+				<div class="destaque hidden-lg hidden-md hidden-sm" style="background: url('<?= RAIZSITE ?>/img/marcacao-modelo-dyna.jpg'); background-size: cover; background-position: center center; border-radius: 15px; height: 285px; position: absolute; width: 100%"></div>
+			</div>
+
+			<div class="destaque hidden-xs" style="background: url('<?= RAIZSITE ?>/img/marcacao-modelo-dyna.jpg'); background-size: cover; background-position: center center; border-radius: 15px; height: 285px; position: absolute; top: 60px">
+				<a role="button" class="btn btn-modelo position-center">Escolha a sua</a>
+			</div>
+		</div>
+	</div>
+
+	<div class="row row-modelo">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+			<h4 class="titulo-harley">harley-davidson</h4>
+			<p class="subtitulo-harley">SPORTSTER</p>
+			
+			<div class="Open-Sans Light size14 Preto-Fonte conteudo">
+				<p>A Sportster é a linha de motos Harley-Davidson com mais tempo na história do motociclismo. Surgiu em 1957, para substituir o modelo K, possuía 850cc e 55 polegadas.</p>
+
+				<p>Chamada de "Sporty", o modelo fez muito sucesso nas pistas de corrida, e também ganhou notoriedade e admiração fora delas, sendo admirada não só por corredores, mas por celebridades como Elvis Presley, que possuiu mais de 20 modelos dessas motocicletas. Por esse motivo elas também ficaram conhecidas como Elvis Bikes.</p>
+
+				<p>A Harley Davidson Sportster é a única linha de motos que mantém a denominação XL e os traços básicos comparáveis com os modelos que as originaram.</p>
+
+				<p>No mercado brasileiro, a família não é completa como nos Estados Unidos, que tem em linha hoje a Superlow, a 883 Iron, a Sevent-Two, Forty Eigth, 1200 Custom, Superlow 1200T, e a Roadster.</p>
+			</div>
+		</div>
+
+		<div class="hidden-xs hidden-sm hidden-md col-lg-6 div-tour" style="margin-top: 40px">
+			<div class="fundo" style="background-color: #000000; position: relative; height: 265px; border-radius: 15px; text-align: center">
+				<svg class="rotate" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" width="20px" height="20px" viewBox="0 0 357 357" style="enable-background:new 0 0 357 357; position: absolute; top: 20px; left: 25px" xml:space="preserve"><g><g id="play-arrow"><polygon points="38.25,0 38.25,357 318.75,178.5   " fill="#f7a600"/></g></g></svg>
+
+				<p style="font-family: 'Geared Slab' !important; text-transform: uppercase; color: #ffbc1b; font-size: 20px; padding-top: 18px">XL 1200</p>
+				
+				<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" width="20px" height="20px" viewBox="0 0 357 357" style="enable-background:new 0 0 357 357; position: absolute; top: 20px; right: 25px" xml:space="preserve"><g><g id="play-arrow"><polygon points="38.25,0 38.25,357 318.75,178.5   " fill="#f7a600"/></g></g></svg>
+
+				<div class="destaque hidden-lg hidden-md hidden-sm" style="background: url('<?= RAIZSITE ?>/img/marcacao-modelo-sportster.jpg'); background-size: cover; background-position: center center; border-radius: 15px; height: 285px; position: absolute; width: 100%"></div>
+			</div>
+
+			<div class="destaque hidden-xs" style="background: url('<?= RAIZSITE ?>/img/marcacao-modelo-sportster.jpg'); background-size: cover; background-position: center center; border-radius: 15px; height: 285px; position: absolute; top: 60px">
+				<a role="button" class="btn btn-modelo position-center">Escolha a sua</a>
+			</div>
+		</div>
+	</div>
+
+	<div class="row row-modelo">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+			<h4 class="titulo-harley">harley-davidson</h4>
+			<p class="subtitulo-harley">SOFTAIL</p>
+			
+			<div class="Open-Sans Light size14 Preto-Fonte conteudo">
+				<p>A principal característica da Softail são as molas e amortecedores traseiros ocultos sob a caixa de transmissão, lembrando o visual das antigas Harleys que não possuíam suspensão traseira e eram chamadas de hardtail (rabo-duro). O nome Softail (rabo macio) foi justamente para designar uma motocicleta com suspensão traseira que, por não estar aparente, tratou de resgatar uma importante referência estética da marca.</p>
+
+				<p>A FXST, lançada em 1983, foi primeira motocicleta da linha Softail. Foi um projeto inovador, que lançou o novo motor Evolution de 1340cc, considerado um grande marco na história da Harley-Davidson.</p>
+
+				<p>O curioso é o que o projeto da Softail não nasceu dentro da Harley-Davidson. O criador foi um engenheiro que queria resgatar o modelo hardtail, mas com a suspensão escondida, possibilitando o uso das motocicletas em viagens longas.</p>
+
+				<p>Depois de diversas tentativas, Bill Davis – o engenheiro – atingiu o resultado que desejava e vendeu a patente e as ferramentas para que a Harley-Davidson pudesse produzir essa família de motocicletas em larga escala.</p>
+			</div>
+		</div>
+
+		<div class="hidden-xs hidden-sm hidden-md col-lg-6 div-tour" style="margin-top: 40px">
+			<div class="fundo" style="background-color: #000000; position: relative; height: 265px; border-radius: 15px; text-align: center">
+				<svg class="rotate" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" width="20px" height="20px" viewBox="0 0 357 357" style="enable-background:new 0 0 357 357; position: absolute; top: 20px; left: 25px" xml:space="preserve"><g><g id="play-arrow"><polygon points="38.25,0 38.25,357 318.75,178.5   " fill="#f7a600"/></g></g></svg>
+
+				<p style="font-family: 'Geared Slab' !important; text-transform: uppercase; color: #ffbc1b; font-size: 20px; padding-top: 18px">FAT BOY SPECIAL</p>
+				
+				<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" width="20px" height="20px" viewBox="0 0 357 357" style="enable-background:new 0 0 357 357; position: absolute; top: 20px; right: 25px" xml:space="preserve"><g><g id="play-arrow"><polygon points="38.25,0 38.25,357 318.75,178.5   " fill="#f7a600"/></g></g></svg>
+
+				<div class="destaque hidden-lg hidden-md hidden-sm" style="background: url('<?= RAIZSITE ?>/img/marcacao-modelo-softail.jpg'); background-size: cover; background-position: center center; border-radius: 15px; height: 285px; position: absolute; width: 100%"></div>
+			</div>
+
+			<div class="destaque hidden-xs" style="background: url('<?= RAIZSITE ?>/img/marcacao-modelo-softail.jpg'); background-size: cover; background-position: center center; border-radius: 15px; height: 285px; position: absolute; top: 60px">
+				<a role="button" class="btn btn-modelo position-center">Escolha a sua</a>
+			</div>
+		</div>
+	</div>
+
+	<div class="row row-modelo">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+			<h4 class="titulo-harley">harley-davidson</h4>
+			<p class="subtitulo-harley">TOURING</p>
+			
+			<div class="Open-Sans Light size14 Preto-Fonte conteudo">
+				<p>A Touring é a linha da Harley-Davidson feita especialmente para as estradas. Ela possui características para longas viagens: como grande capacidade de carga, para-brisas e sistemas de som. Os dois modelos de maior destaque dessa família são a Road King e a Electra Glide.</p>
+
+				<p>Foram realizadas diversas modificações e melhorias nos motores da Touring, mas sempre foi mantida a arquitetura tradicional dos dois cilindros em V e refrigeração a ar, de construção bastante simples e robusta.</p>
+
+				<p>As motocicletas da família da Touring são grandes e pesadas, tudo para dar todo o conforto para o piloto e para o passageiro durante as viagens. Ela vem equipada com grandes bolsas laterais que são a sua marca registrada.</p>
+
+				<p>A polícia rodoviária americana adotou alguns modelos da linha Touring para compor sua frota, imortalizando esse estilo de motocicletas em diversos filmes em que aparece um policial rodoviário pilotando um modelo Touring.</p>
+			</div>
+		</div>
+
+		<div class="hidden-xs hidden-sm hidden-md col-lg-6 div-tour" style="margin-top: 40px">
+			<div class="fundo" style="background-color: #000000; position: relative; height: 265px; border-radius: 15px; text-align: center">
+				<svg class="rotate" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" width="20px" height="20px" viewBox="0 0 357 357" style="enable-background:new 0 0 357 357; position: absolute; top: 20px; left: 25px" xml:space="preserve"><g><g id="play-arrow"><polygon points="38.25,0 38.25,357 318.75,178.5   " fill="#f7a600"/></g></g></svg>
+
+				<p style="font-family: 'Geared Slab' !important; text-transform: uppercase; color: #ffbc1b; font-size: 20px; padding-top: 18px">ELECTRA GLIDE ULTRA LIMITED</p>
+				
+				<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" width="20px" height="20px" viewBox="0 0 357 357" style="enable-background:new 0 0 357 357; position: absolute; top: 20px; right: 25px" xml:space="preserve"><g><g id="play-arrow"><polygon points="38.25,0 38.25,357 318.75,178.5   " fill="#f7a600"/></g></g></svg>
+
+				<div class="destaque hidden-lg hidden-md hidden-sm" style="background: url('<?= RAIZSITE ?>/img/marcacao-modelo-touring.jpg'); background-size: cover; background-position: center center; border-radius: 15px; height: 285px; position: absolute; width: 100%"></div>
+			</div>
+
+			<div class="destaque hidden-xs" style="background: url('<?= RAIZSITE ?>/img/marcacao-modelo-touring.jpg'); background-size: cover; background-position: center center; border-radius: 15px; height: 285px; position: absolute; top: 60px">
+				<a role="button" class="btn btn-modelo position-center">Escolha a sua</a>
+			</div>
+		</div>
+	</div>
+
+	<div class="row row-modelo">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+			<h4 class="titulo-harley">harley-davidson</h4>
+			<p class="subtitulo-harley">V-ROD</p>
+			
+			<div class="Open-Sans Light size14 Preto-Fonte conteudo">
+				<p>A V-Rod surgiu em 2001 e foi desenvolvida para fazer frente às marcas japonesas. É um modelo personalizado de motocicletas de alto desempenho da Harley-Davidson, com o motor Revolution desenvolvido em parceria com a Porsche. A V-Rod utiliza, pela primeira vez, refrigeração líquida. O tanque de combustível está localizado abaixo do assento, ao invés de ficar na parte da frente, inspirado na Superbike VR-1000, também da Harley.</p>
+
+				<p>Todos os modelos da V-Rod são produzidos em Missouri (Kansas), na fábrica de veículos e operações de Powertrain. Sucesso absoluto no Brasil, o modelo utiliza um motor mais silencioso, refrigerado a água.</p>
+
+				<p>Seguindo o conceito de Power-cruise, a V-Rod é facilmente reconhecida pelo motor V-Twin de 60 graus. Motor com 115cv de potência e que chega em 8.250 RPM antes de entrar na faixa vermelha, que atinge 9.000 RPM.</p>
+			</div>
+		</div>
+
+		<div class="hidden-xs hidden-sm hidden-md col-lg-6 div-tour" style="margin-top: 40px">
+			<div class="fundo" style="background-color: #000000; position: relative; height: 265px; border-radius: 15px; text-align: center">
+				<svg class="rotate" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" width="20px" height="20px" viewBox="0 0 357 357" style="enable-background:new 0 0 357 357; position: absolute; top: 20px; left: 25px" xml:space="preserve"><g><g id="play-arrow"><polygon points="38.25,0 38.25,357 318.75,178.5   " fill="#f7a600"/></g></g></svg>
+
+				<p style="font-family: 'Geared Slab' !important; text-transform: uppercase; color: #ffbc1b; font-size: 20px; padding-top: 18px">NIGHT ROD SPECIAL</p>
+				
+				<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" width="20px" height="20px" viewBox="0 0 357 357" style="enable-background:new 0 0 357 357; position: absolute; top: 20px; right: 25px" xml:space="preserve"><g><g id="play-arrow"><polygon points="38.25,0 38.25,357 318.75,178.5   " fill="#f7a600"/></g></g></svg>
+
+				<div class="destaque hidden-lg hidden-md hidden-sm" style="background: url('<?= RAIZSITE ?>/img/marcacao-modelo-v-rod.jpg'); background-size: cover; background-position: center center; border-radius: 15px; height: 285px; position: absolute; width: 100%"></div>
+			</div>
+
+			<div class="destaque hidden-xs" style="background: url('<?= RAIZSITE ?>/img/marcacao-modelo-v-rod.jpg'); background-size: cover; background-position: center center; border-radius: 15px; height: 285px; position: absolute; top: 60px">
+				<a role="button" class="btn btn-modelo position-center">Escolha a sua</a>
+			</div>
 		</div>
 	</div>
 </div>
